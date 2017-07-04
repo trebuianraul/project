67a5415d6c05f4a4141076c2c54cca785129cac0
raul.trebuian <raul.trebuian@RODSK41018>
2017-07-04 16:56:33 +0300

    commit1

---
+++ one/one/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("try 1");
+        }
+    }
+}
