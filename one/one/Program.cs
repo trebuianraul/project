0c381cd661b29c34b179414c95271bf94cb56582
raul.trebuian <raul.trebuian@RODSK41018>
2017-07-04 17:16:25 +0300

    c4

--- one/one/Program.cs
+++ one/one/Program.cs
@@ -10,7 +10,7 @@ namespace one
         static void Main(string[] args)
         {
             Console.WriteLine("try 1");
-            Console.WriteLine("hi");
+            Console.WriteLine("h");
         }
     }
 }
